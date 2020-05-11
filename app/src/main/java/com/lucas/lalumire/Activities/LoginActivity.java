8f1244a64906021d9bb104bcfb9a9509f991aeff
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2020-05-11 15:39:38 +0800

    Koin: JavaAppKoin: Optimize imports

--- app/src/main/java/com/lucas/lalumire/Activities/LoginActivity.java
+++ app/src/main/java/com/lucas/lalumire/Activities/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.lucas.lalumire.Activities;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
--- app/src/main/java/com/lucas/lalumire/koin/JavaAppKoin.kt
+++ app/src/main/java/com/lucas/lalumire/koin/JavaAppKoin.kt
@@ -1,12 +1,12 @@
 package com.lucas.lalumire.koin
 
-import android.app.Application
 import com.lucas.lalumire.Repositories.FirebaseAuthRepository
 import com.lucas.lalumire.Viewmodels.LoginViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.dsl.module
+
 @JvmField
 val module : Module = module{
     //singleton because we cannot use kotlin and take advantage of property delegation for sharedViewModel
