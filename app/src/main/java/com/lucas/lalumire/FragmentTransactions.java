88a6f93c70804e52f990605b169b2659b73ee601
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2020-05-14 12:53:28 +0800

    FragmentTransactions: documentation
    Signed-off-by: Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>

--- app/src/main/java/com/lucas/lalumire/FragmentTransactions.java
+++ app/src/main/java/com/lucas/lalumire/FragmentTransactions.java
@@ -8,15 +8,34 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import android.R.*;
 public class FragmentTransactions {
+    /**
+     * Function that launches fragment and puts it into the defined container
+     * @param fragment new instance of fragment i.e. new LoginFragment()
+     * @param container the container that will host the fragment i.e. R.id.constraintLayout
+     * @param context the context of the activity i.e getApplicationContext()
+     * @param animation true/false to specify if animation is needed
+     */
     public static void LaunchFragment(@NonNull Fragment fragment, int container, @NonNull FragmentActivity context, boolean animation){
+        //get fragment manager
         FragmentManager manager = context.getSupportFragmentManager();
+        //begin the transaction
         FragmentTransaction transaction = manager.beginTransaction();
+        //replace the fragment into the container, container is just a layout which the fragment will reside in
         transaction.replace(container, fragment);
         if(animation){
             transaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right, android.R.anim.slide_in_left, android.R.anim.slide_out_right );
         }
         transaction.commit();
     }
+
+    /**
+     * Function that replaces fragment and adds it to the backstack
+     * @param fragment new instance of fragment i.e. new LoginFragment()
+     * @param container the container that will host the fragment i.e. R.id.constraintLayout
+     * @param context the context of the activity i.e getApplicationContext()
+     * @param backState the previous fragment's name
+     * @param animation true/false to specify if animation is needed
+     */
     public static void ReplaceFragment(@NonNull Fragment fragment, int container, @NonNull FragmentActivity context, @NonNull String backState, boolean animation){
         FragmentManager manager = context.getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
