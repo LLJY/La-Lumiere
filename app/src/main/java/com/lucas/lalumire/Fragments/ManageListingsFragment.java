86d84fffea2625894a97cf88b402ada28e6a6b8f
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2020-05-14 13:40:17 +0800

    add Manage Listings fragment and View Model
    for seller screen
    
    Signed-off-by: Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>

---
+++ app/src/main/java/com/lucas/lalumire/Fragments/ManageListingsFragment.java
@@ -0,0 +1,39 @@
+package com.lucas.lalumire.Fragments;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lucas.lalumire.R;
+import com.lucas.lalumire.Viewmodels.ManageListingsViewModel;
+
+public class ManageListingsFragment extends Fragment {
+
+    private ManageListingsViewModel mViewModel;
+
+    public static ManageListingsFragment newInstance() {
+        return new ManageListingsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.manage_listings_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ManageListingsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/lucas/lalumire/Viewmodels/ManageListingsViewModel.java
@@ -0,0 +1,7 @@
+package com.lucas.lalumire.Viewmodels;
+
+import androidx.lifecycle.ViewModel;
+
+public class ManageListingsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
