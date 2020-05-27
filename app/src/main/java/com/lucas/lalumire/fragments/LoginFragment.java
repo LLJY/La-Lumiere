a7fa06f656a6195abfe1ef10b9cd4d4747a1416c
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2020-05-27 14:32:51 +0800

    animation!
    Signed-off-by: Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>

--- app/src/main/java/com/lucas/lalumire/fragments/LoginFragment.java
+++ app/src/main/java/com/lucas/lalumire/fragments/LoginFragment.java
@@ -1,18 +1,22 @@
 package com.lucas.lalumire.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 
 import com.lucas.lalumire.activities.LoginActivity;
 import com.lucas.lalumire.R;
+import com.lucas.lalumire.databinding.SplashScreenBinding;
 import com.lucas.lalumire.viewmodels.LoginViewModel;
 import com.lucas.lalumire.databinding.LoginFragmentBinding;
 
@@ -20,6 +24,7 @@ import java.util.Objects;
 
 import kotlin.Lazy;
 
+import static org.koin.java.KoinJavaComponent.bind;
 import static org.koin.java.KoinJavaComponent.inject;
 
 public class LoginFragment extends Fragment {
@@ -28,6 +33,7 @@ public class LoginFragment extends Fragment {
     private Lazy<LoginViewModel> loginViewModel = inject(LoginViewModel.class);
     //setup View Binding.
     private LoginFragmentBinding binding;
+    private SplashScreenBinding splashScreenBinding;
 
     public LoginFragment() {
 
@@ -47,6 +53,32 @@ public class LoginFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        final ConstraintSet constraintSet1 = new ConstraintSet();
+        constraintSet1.clone(binding.constraintLayout);
+        ConstraintSet constraintSet2 = new ConstraintSet();
+        constraintSet2.clone(getContext(), R.layout.splash_screen);
+        // set the starting splash screen
+        TransitionManager.beginDelayedTransition(binding.constraintLayout);
+        constraintSet2.applyTo(binding.constraintLayout);
+        binding.emailBox.setVisibility(View.INVISIBLE);
+        binding.passwordText.setVisibility(View.INVISIBLE);
+        binding.loginButton.setVisibility(View.INVISIBLE);
+        binding.googleButton.setVisibility(View.INVISIBLE);
+        binding.materialTextView.setVisibility(View.INVISIBLE);
+        // delay for 3s before setting view visible
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                TransitionManager.beginDelayedTransition(binding.constraintLayout);
+                constraintSet1.applyTo(binding.constraintLayout);
+                binding.emailBox.setVisibility(View.VISIBLE);
+                binding.passwordText.setVisibility(View.VISIBLE);
+                binding.loginButton.setVisibility(View.VISIBLE);
+                binding.googleButton.setVisibility(View.VISIBLE);
+                binding.materialTextView.setVisibility(View.VISIBLE);
+            }
+        }, 1600);
+
         //set the username text from viewmodel, worth noting that this is actually email.
         binding.emailBox.getEditText().setText(loginViewModel.getValue().loginUsername);
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
